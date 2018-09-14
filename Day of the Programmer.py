464e7ce5c5806a8e824c8cd2cd183b286e2ee554
AnushaS06 <40312715+AnushaS06@users.noreply.github.com>
2018-09-14 13:42:07 +0530

    Create Day of the Programmer.py

---
+++ Day of the Programmer.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the dayOfProgrammer function below.
+def dayOfProgrammer(year):
+    if(year==1918):
+        return('26.09.%s' %year)
+    if((year>=1919) and year%400 == 0 or (year%4==0 and year%100 !=0)):
+        return('12.09.%s' %year)
+    if((year<=1917) and year%4 == 0):
+        return('12.09.%s' %year)
+    return('13.09.%s' %year)
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
