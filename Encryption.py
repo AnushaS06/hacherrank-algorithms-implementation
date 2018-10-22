89dc17b179237e8e743379ea8f0122a2590105bf
Anusha S <40312715+AnushaS06@users.noreply.github.com>
2018-10-22 19:42:55 +0530

    Create Encryption.py

---
+++ Encryption.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the encryption function below.
+def encryption(s):
+    s = s.replace(" ","")
+    x = math.sqrt(len(s))
+    y=int(x)
+    if (x!=y):
+        i1=y
+        j1=i1+1
+    else:
+        i1=y
+        j1=i1
+    print(len(s),x,i1,j1)
+    res = []
+    ret =[]
+    for i in range(j1):
+        res.append(s[i::j1]+" ")
+        #res.append(" ")
+    return(res)
+    for i in res:
+        ret.append(i)
+        ret=list(ret)
+    print(ret)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = (encryption(s))
+
+    fptr.writelines(result)
+
+    fptr.close()
